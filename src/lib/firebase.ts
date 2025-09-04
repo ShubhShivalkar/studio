95f1cce96f2c02e4c619935e54fb28ce54e14e92
Shubham Shivalkar <shivalkarshubham1@gmail.com>
2025-09-04 21:31:10 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB8t1nn61fQUfSdPxG3S_EMPvwGWVb3pnA",
+  authDomain: "anubhav-anuvaad.firebaseapp.com",
+  projectId: "anubhav-anuvaad",
+  storageBucket: "anubhav-anuvaad.firebasestorage.app",
+  messagingSenderId: "954897713031",
+  appId: "1:954897713031:web:0e53e59ae6e4768f777522",
+  measurementId: "G-4ZXZLG112G"
 };
 
 // Initialize Firebase
