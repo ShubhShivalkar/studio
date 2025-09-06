af6b21553d37aa83d1987b5bd1d554a83aeeb745
Shubham Shivalkar <shivalkarshubham1@gmail.com>
2025-09-06 09:26:03 +0000

    how to configure turbopack properly

---
+++ src/next.config.ts
@@ -0,0 +1,50 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+        ],
+      },
+    ];
+  },
+  // Explicitly add the turbo object for Turbopack configuration
+  turbo: {},
+  experimental: {
+    // This resolves the cross-origin request warning in the console
+    allowedDevOrigins: [
+        "https://*.cluster-mwsteha33jfdowtvzffztbjcj6.cloudworkstations.dev",
+    ],
+  },
+};
+
+export default nextConfig;
